4d337c96a20dcbae027593f7d75b53fe38632c06
ravi-aratchige <rmaratchige@gmail.com>
2022-11-05 00:55:54 +0530

    Added responsivity for changing browser screen sizes (desktop only)
    Added basic responsivity (desktop only)
    Changed background image
    Fixed margin and whitespace errors

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function hideCookieBar() {
     document.querySelector('.overlay').style.display = "none";
     document.querySelector('.cookieBar').style.display = "none";
 }
+
+// adjusting height of right side based on height of left side to ensure responsivity
+
+let leftSideHeight = document.querySelector('.leftSide').clientHeight;
+let rightSideHeight = document.querySelector('.rightSide').clientHeight;
+
+if (leftSideHeight > rightSideHeight) {
+    rightSideHeight = leftSideHeight + "px";
+} else {
+    leftSideHeight = rightSideHeight + "px";
+}
